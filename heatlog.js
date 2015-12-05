dabd2c7f5083152dadfebbbb64484cb2af00a2d7
Bob Stachel <rstachel@gmail.com>
2015-12-05 14:21:00 -0500

    addded base ajax scripting

--- heatlog.js
+++ heatlog.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
 
+var heatlogurlprefix = 'https://rstachel.cartodb.com/api/v2/sql?q=';
+
+var heatlogurlsuffix = '&api_key=146d6f56877a3f50678a19387e10fbfda8b0de30';
+
 
   $('.submit-button').on(click, function(){
     debugger;
   })
 
 
-  function ajaxCall() {
-    var input = $("#text-field").val();
+  function heatLogSubmitAjaxCall(data, callback) 
 
     $.ajax({
-      url: "/widget",
-      data: { search_keyword: input },
+      url: heatlogurlprefix + encodeURI(data) + heatlogurlsuffix;
+      data: { },
     })
-      .done(function( youTubeWidget ) {
-        // replace with the new video
-        $("#search-results").html(youTubeWidget);
-      });
+      .done(function(json){callback(json)} );
   }
 
 });
