248cc135ba11b887c0a2f9b74e6c4452ec87d80b
hwinn4 <hilary.winn@gmail.com>
2016-01-02 16:51:04 -0500

    refactors methods

--- app/js/landing-page.js
+++ app/js/landing-page.js
@@ -32,48 +32,42 @@ app.landing = (function(w,d,$) {
       }
     } 
 
-    function getWeather() {
+    function insideMinTemp(currentTemp){
+      var minTemp = false;
+      if (currentMilitaryHour > 6 && currentMilitaryHour < 22 && currentTemp < 55) {
+        var minTemp = 68 + "˚F"; 
+      }
+      else if (currentMilitaryHour > 22 && currentTemp < 40 || currentMilitaryHour < 6 && currentTemp < 40) {
+        var minTemp = 55 + "˚F";
+      }
+      return minTemp;
+    }
+
+    function displayTemperatureInfo() {
       $.getJSON("//api.openweathermap.org/data/2.5/weather?q=newyork&units=imperial&APPID=9b7b2c28d9900e12be5ff93fe2677b09", function(data){
         var currentTemp = data.main.temp;
         // currentTemp = 56;
-        var outsideTemp = insideMinTemp();    
+        var $temperatureInfo = $('#temperature-info');
+        var insideTemperature = insideMinTemp(currentTemp);
 
         currentTemp += "˚F";
-        
-        $('.outside-temp').text(currentTemp);
 
-        if (!isHeatSeason){
-          $('#temperature-info').html("<h1>According to NYC law, there is no minimum temperature at the moment. The regulations are only in effect from October 1 to May 31.</h1>");
-        }
-        else if (outsideTemp && isHeatSeason){
-          $('.inside-temp').text(outsideTemp);
+        if (currentMonth > 5 && currentMonth < 10) {
+          $temperatureInfo.html("<h1>According to NYC law, there is no minimum temperature at the moment. The regulations are only in effect from October 1 to May 31.</h1>");
         } 
-        else {
-          $('#temperature-info').html("<h1>According to NYC law, there is no minimum temperature at the moment. It is too warm outside for regulations to take effect.</h1>");
+        else if (insideTemperature) {
+          $('.time').text(theTime);
+          $('.outside-temp').text(currentTemp);
+          $('.inside-temp').text(insideTemperature);
         }
-
-        function insideMinTemp(){
-          if (currentMonth > 5 && currentMonth < 10) {
-            isHeatSeason = false;
-            var minTemp = false;
-          }
-          else if (currentMilitaryHour > 6 && currentMilitaryHour < 22 && currentTemp < 55) {
-            var minTemp = 68 + "˚F"; 
-          }
-          else if (currentMilitaryHour > 22 && currentTemp < 40 || currentMilitaryHour < 6 && currentTemp < 40) {
-            var minTemp = 55 + "˚F";
-          }
-          else {
-            var minTemp = false;
-          }
-          return minTemp;
+        else {
+          $temperatureInfo.html("<h1>According to NYC law, there is no minimum temperature at the moment. It is too warm outside for regulations to take effect.</h1>");
         }
       });
     }
 
     function init(){
-      $('.time').text(theTime);
-      getWeather();
+      displayTemperatureInfo();
     }
 
     return {
